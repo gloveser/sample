607304a1a5cfd44097fec1aee09137104c4a0d58
vagrant <vagrant@homestead>
2018-04-02 09:07:39 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UsersController extends Controller
 {
-    //
+    
+    //增加
       public function create()
     {
         return view('users.create');
+    }
+
+     //展示
+     public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+     //新增
+     public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+       //等于 redirect()->route('users.show', [$user->id]);moren 
+        return redirect()->route('users.show', [$user]);
+
+
     }
 }
